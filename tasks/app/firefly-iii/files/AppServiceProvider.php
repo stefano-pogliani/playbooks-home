8052d3417ee19e1f9ec58de285a4d6cd8c07e587
Stefano Pogliani <stefano@spogliani.net>
2018-12-29 15:04:43 +0100

    Downgrade firefly to 4.7.8 and fix HTTPS detection

---
+++ tasks/app/firefly-iii/files/AppServiceProvider.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * AppServiceProvider.php
+ * Copyright (c) 2017 thegrumpydictator@gmail.com
+ *
+ * This file is part of Firefly III.
+ *
+ * Firefly III is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Firefly III is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Firefly III. If not, see <http://www.gnu.org/licenses/>.
+ */
+declare(strict_types=1);
+
+namespace FireflyIII\Providers;
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Passport;
+use URL;
+
+/**
+ * @codeCoverageIgnore
+ * Class AppServiceProvider.
+ */
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Schema::defaultStringLength(191);
+        // FIX(stefano): pathed through DockerFile because proxies are hard.
+        //if ('heroku' === config('app.env')) {
+            URL::forceScheme('https');
+        //}
+    }
+
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        Passport::ignoreMigrations();
+    }
+}
